7fc0b96635603fb4195912558f54f7973d173b1e
Endeyr <aaron.thompson42594@gmail.com>
2022-10-13 11:09:10 -0400

    add display inputs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React, { Component } from 'react'
 import './App.css'
 import { Display } from './components/Display'
 
-function App() {
-	return (
-		<main className="container">
-			<h3 className="text-center">CV Creator</h3>
-			<div>
-				<Display />
-			</div>
-		</main>
-	)
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<main className="container">
+				<h3 className="text-center">CV Creator</h3>
+				<div>
+					<Display />
+				</div>
+			</main>
+		)
+	}
 }
 
 export default App
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,61 +1,103 @@
 export function Display() {
+	const onSubmit = (e) => {
+		e.preventDefault()
+	}
+
 	return (
-		<div>
+		<form onSubmit={onSubmit}>
 			<section>
 				<h3>General Info</h3>
 				<div className="input-group mb-3">
-					<span className="input-group-text">Name</span>
+					<label htmlFor="name" className="input-group-text">
+						Name
+					</label>
 					<input type="text" className="form-control" />
 				</div>
 				<div className="input-group mb-3">
-					<span className="input-group-text">Email</span>
+					<label htmlFor="email" className="input-group-text">
+						Email
+					</label>
 					<input type="email" className="form-control" />
 				</div>
 				<div className="input-group mb-3">
-					<span className="input-group-text">Phone Number</span>
-					<input type="tel" className="form-control" />
+					<label htmlFor="phone" className="input-group-text">
+						Phone Number
+					</label>
+					<input
+						type="tel"
+						className="form-control"
+						pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+					/>
+				</div>
+				<div className="input-group mb-3">
+					<label htmlFor="address" className="input-group-text">
+						Mailing Address
+					</label>
+					<input type="text" className="form-control" placeholder="street" />
+					<input type="text" className="form-control" placeholder="city" />
+					<input type="text" className="form-control" placeholder="state" />
+					<input type="text" className="form-control" placeholder="zipcode" />
 				</div>
 			</section>
 			<section>
 				<h3>Educational Experience</h3>
 				<div className="input-group mb-3">
-					<span className="input-group-text">School Name</span>
+					<label htmlFor="school" className="input-group-text">
+						School Name
+					</label>
 					<input type="text" className="form-control" />
 				</div>
 				<div className="input-group mb-3">
-					<span className="input-group-text">Field of Study</span>
+					<label htmlFor="degree" className="input-group-text">
+						Field of Study
+					</label>
 					<input type="email" className="form-control" />
 				</div>
 				<div className="input-group mb-3">
-					<span className="input-group-text">Graduation Date</span>
+					<label htmlFor="graduation" className="input-group-text">
+						Graduation Date
+					</label>
 					<input type="month" className="form-control" />
 				</div>
 			</section>
 			<section>
 				<h3>Practical Experience</h3>
 				<div className="input-group mb-3">
-					<span className="input-group-text">Company Name</span>
+					<label htmlFor="company" className="input-group-text">
+						Company Name
+					</label>
 					<input type="text" className="form-control" />
 				</div>
 				<div className="input-group mb-3">
-					<span className="input-group-text">Title</span>
+					<label htmlFor="title" className="input-group-text">
+						Title
+					</label>
 					<input type="text" className="form-control" />
 				</div>
 				<div className="input-group mb-3">
-					<span className="input-group-text">Duties</span>
+					<label htmlFor="duties" className="input-group-text">
+						Duties
+					</label>
 					<input type="text" className="form-control" />
 					<input type="text" className="form-control" />
 					<input type="text" className="form-control" />
 				</div>
 				<div className="input-group mb-3">
-					<span className="input-group-text">Dates of Employment</span>
+					<label htmlFor="employment" className="input-group-text">
+						Dates of Employment
+					</label>
 					<input type="date" className="form-control" />
 					<input type="date" className="form-control" />
 				</div>
 			</section>
-			<button type="button" className="btn btn-primary">
-				Submit
-			</button>
-		</div>
+			<div className="d-flex justify-content-between">
+				<button type="button" className="btn btn-secondary mb-3">
+					Add Experience
+				</button>
+				<button type="submit" className="btn btn-primary mb-3">
+					Submit
+				</button>
+			</div>
+		</form>
 	)
 }
