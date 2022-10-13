02bad43578b6769c724d761e4d5bcfdf735bdf06
Endeyr <aaron.thompson42594@gmail.com>
2022-10-13 09:36:18 -0400

    add basic format

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,3 +1,58 @@
 export function Display() {
-	return <div>Display Component</div>
+	return (
+		<div>
+			<section>
+				<h3>General Info</h3>
+				<div className="input-group mb-3">
+					<span className="input-group-text">Name</span>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="input-group mb-3">
+					<span className="input-group-text">Email</span>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="input-group mb-3">
+					<span className="input-group-text">Phone Number</span>
+					<input type="text" className="form-control" />
+				</div>
+			</section>
+			<section>
+				<h3>Educational Experience</h3>
+				<div className="input-group mb-3">
+					<span className="input-group-text">School Name</span>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="input-group mb-3">
+					<span className="input-group-text">Field of Study</span>
+					<input type="email" className="form-control" />
+				</div>
+				<div className="input-group mb-3">
+					<span className="input-group-text">Graduation Date</span>
+					<input type="text" className="form-control" />
+				</div>
+			</section>
+			<section>
+				<h3>Practical Experience</h3>
+				<div className="input-group mb-3">
+					<span className="input-group-text">Company Name</span>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="input-group mb-3">
+					<span className="input-group-text">Title</span>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="input-group mb-3">
+					<span className="input-group-text">Task</span>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="input-group mb-3">
+					<span className="input-group-text">Dates of Employment</span>
+					<input type="text" className="form-control" />
+				</div>
+			</section>
+			<button type="button" className="btn btn-primary">
+				Submit
+			</button>
+		</div>
+	)
 }
